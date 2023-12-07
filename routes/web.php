930faee0b510e2686b4762ee7e15296802696816
[Oriol Esquena Calm] <oesquena@uoc.edu>
2023-12-07 15:19:57 +0100

    create controller and edit web route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AngularController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::any('/{any}', [AngularController::class, 'index'])-> where('any', '^(?!api).*$');
+
+
+
+/*
+get('/', function () {
     return view('welcome');
 });
+*/
